9820fe211a6e1050cb4f9c2891b63fbf917a4db7
knightsdex <stard8447@gmail.com>
2025-11-10 10:09:05 -0800

    updated

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
-
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
     plugins: {
       "react-hooks": reactHooks,
